3cd27b246c48baff50457cc63892caa11df3733b
wangocho <wangocho11>
2020-02-08 11:52:13 +0300

    first commit

---
+++ src/test/java/com/budgetapp/backend/BudgetappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
